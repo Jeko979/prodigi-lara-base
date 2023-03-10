8e211b37469cc74424ca53564bf61a36b6638de6
Jacopo Rosi <jacoporosi3@gmail.com>
2023-03-10 15:33:59 +0100

    Creazione in invoices  dei campi  code, total e payment

--- database/migrations/2023_03_10_142630_create_invoices_table.php
+++ database/migrations/2023_03_10_142630_create_invoices_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->integer('code');
             $table->integer('total');
+            $table->string('payment',50);
             $table->timestamps();
         });
     }
