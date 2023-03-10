439e871b1e470065cdbf1c3c840681668ba02321
Jacopo Rosi <jacoporosi3@gmail.com>
2023-03-10 15:24:16 +0100

    cancellati controller e viste

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\clientiController;
-use App\Http\Controllers\ordiniController;
-use App\Http\Controllers\fattureController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,7 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/clienti',[clientiController::class, 'prova']);
-Route::get('/ordini',[ordiniController::class, 'prova']);
-Route::get('/fatture',[fattureController::class, 'prova']);
